5c58a3105120f45a4378116c1aafcf1ab3fb9fc3
Bhaskar <bhaskar@cowhite.com>
2018-04-26 15:42:42 +0530

    Added membership and roles models.

--- django_roles/models.py
+++ django_roles/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
 
-# Create your models here.
+
+class DateTimeBase(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Role(DateTimeBase):
+    name = models.CharField(max)
+    description = models.TextField(null=True, blank=True)
+
+
+class GenericMember(DateTimeBase):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    role = models.ForeignKey(Role, null=True, blank=True)
+
+
+class BulkInvitation(DateTimeBase):
+    emails = models.TextField()
+
+
+class MembershipInvitation(DateTimeBase):
+    code = models.UUIDField()
+    email = models.EmailField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+    accepted_invitation = models.BooleanField(default=False)
+    accepted_time = models.DateTimeField(null=True, blank=True)
+    invited_by = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
